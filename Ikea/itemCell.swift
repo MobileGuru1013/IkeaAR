92948d37ff98edbcd4d9ad01e35603c21a825e7e
Mobile Guru <dragon@dragons-iMac-2.local>
2018-11-20 11:43:08 +0200

    IkeaAR
    ARKit app for Ikea furnitures

---
+++ Ikea/itemCell.swift
@@ -0,0 +1,15 @@
+//
+//  itemCell.swift
+//  Ikea
+//
+//  Created by Mobile Guru on 2017-08-18.
+//  Copyright © 2017 Mobile Guru. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+ 
+    @IBOutlet weak var itemLabel: UILabel!
+    
+}
